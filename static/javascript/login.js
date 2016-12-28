d56bdb169cf3586387e7e9575e2e8b0ef1d86c2a
opelar <23897631@qq.com>
2016-12-28 19:36:22 +0800

    登陆验证

--- static/javascript/login.js
+++ static/javascript/login.js
@@ -1,25 +1,69 @@
 var BaseLogin = function() {
-	
+
 	// 控制登录注册的切换
 	var initToggler = function() {
-		var login = jQuery('.login-form');
-		var register  = jQuery('.register-form');
+		var login = $('.login-form');
+		var register  = $('.register-form');
 			
-		jQuery('#register').on('click', function(e) {
+		$('#register').on('click', function(e) {
 			e.preventDefault();
 			login.hide();
 			register.show();
 		});
 
-		jQuery('#login').on('click', function(e) {
+		$('#login').on('click', function(e) {
 			e.preventDefault();
 			register.hide();
 			login.show();
 		});
 	};
 
+	// 弹窗
+	var notify = function(text) {
+		var notifys = $('.notifys');
+		var notify = $('<li />').html(text);
+		if(notifys.length == 0){
+			notifys = $('<ul />').addClass('notifys').appendTo('body');
+		}
+		if(notifys.children('li').length > 0)
+			notifys.html('');
+
+		notifys.append(notify);
+
+		notifys.fadeIn(300, function(){
+			$(this).delay(3000).fadeOut(300, function(){
+				$(this).remove();
+			});
+		});
+	};
+
 	// 表单的验证提交
 	var initValidate = function() {
+		// 登录
+		var login_btn = $('#login_btn');
+		var login_form = $('.login-form');
+
+		login_btn.on('click', function(e) {
+			e.preventDefault();
+			
+			var login_username = $('#login_username').val();
+			var login_password = $('#login_password').val();
+			var re = /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/;
+			var len = login_password.length;
+			
+			if (login_username == '') {
+				notify('帐号不能为空！');
+			} else if (!re.test(login_username)) {
+				notify('帐号格式不正确！');
+			} else if (len == 0) {
+				notify('密码不能为空！');
+			} else if (len < 6 || len > 16) {
+				notify('密码为 6 至 16 位');
+			}else {
+				login_form.submit();	
+			}
+
+		});
 
 	};
 
